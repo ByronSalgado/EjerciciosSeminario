2ba28705a5ae4958efbee53f577b7fdf1e4e0fa5
ByronSalgado <byronsalgago25@gmail.com>
2018-01-30 18:37:46 -0600

    Commit Inical de Poyecto

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Salgado' });
+});
+
+router.get('/data', function(req, res, next) {
+  res.render('index', { title: 'Data Uri' });
+});
+
+router.get('/prueba', function(req, res, next) {
+  res.render('index', { title: 'Prueba' });
+});
+
+router.get('/prueba1', function(req, res, next) {
+  res.render('newview', { "Nombre": "Byron",
+                          "Correo":"bsalgado@gmail.com"});
+});
+
+module.exports = router;
